b717a2b51d7c3b74164d6e69540d9f168d21039c
Amit kumar <amitkumar29723@gmail.com>
2026-01-18 23:54:15 +0530

    update substraction

--- addtion_app.py
+++ addtion_app.py
@@ -5,6 +5,6 @@ b=st.number_input("Enter second number:")
 if st.button('Addtion'):
     c=a+b
     st.success(f'{a}+{b}={c}')
-if st.button('Addtion'):
+elif st.button('Substraction'):
     c=a-b
     st.success(f'{a}-{b}={c}')
